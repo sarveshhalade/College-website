67f69bc7490720cdd2b130b2283fc64aef8d8839
sarveshhalade <sdhalade9799@gmail.com>
2025-11-09 16:13:18 +0530

    updated contactpage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -14,7 +14,7 @@ const ContactPage = () => {
 
     try {
       // ✅ Use your deployed backend URL here (Render / Railway)
-      const res = await fetch("https://college-backend.onrender.com/api/contact", {
+      const res = await fetch("https://college-website-0zcl.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
